62a16c448e30d568143b6e4a084148b0939bf106
rdperry031 <rdperry031@gmail.com>
2021-08-13 11:37:24 -0500

    passing all tests, just needs styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from 'react'
+import Styled from 'styled-components'
+
+export default function Character(props) {
+    const {character} = props;
+    return (
+        <div>
+            {character.name}
+        </div>
+    )
+}
